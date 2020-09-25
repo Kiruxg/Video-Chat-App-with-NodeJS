f48de5656ef2fcdec6ea058af1e95127f1c405aa
Kirubel Girma <kirubelgirma0@gmail.com>
2020-09-24 17:22:52 -0700

    homepage css styles

--- public/script.js
+++ public/script.js
@@ -1,166 +1,166 @@
 //JS for front-end
-const socket = io("/");
-const videoGrid = document.getElementById("video-grid");
-const myVideo = document.createElement("video");
-myVideo.muted = true;
+const socket = io("/")
+const videoGrid = document.getElementById("video-grid")
+const myVideo = document.createElement("video")
+myVideo.muted = true
 
 var myPeer = new Peer(undefined, {
   path: "/peerjs",
   host: "/",
-  port: "3001",
-});
-const peers = {};
+  port: "3001"
+})
+const peers = {}
 
-let myVideoStream;
+let myVideoStream
 navigator.mediaDevices
   .getUserMedia({
     video: true,
-    audio: true,
+    audio: true
   })
-  .then((stream) => {
-    console.log("my stream", stream);
+  .then(stream => {
+    console.log("my stream", stream)
     //my video connection
-    myVideoStream = stream;
-    addVideoStream(myVideo, stream);
+    myVideoStream = stream
+    addVideoStream(myVideo, stream)
 
     //they call us after connecting, we add their stream to view
-    myPeer.on("call", (call) => {
-      console.log("answered the call: ", call);
-      call.answer(stream);
-      console.log("call answer pass");
-      const video = document.createElement("video");
-      call.on("stream", (userVideoStream) => {
-        console.log("their stream", userVideoStream);
-        addVideoStream(video, userVideoStream);
-      });
-    });
+    myPeer.on("call", call => {
+      console.log("answered the call: ", call)
+      call.answer(stream)
+      console.log("call answer pass")
+      const video = document.createElement("video")
+      call.on("stream", userVideoStream => {
+        console.log("their stream", userVideoStream)
+        addVideoStream(video, userVideoStream)
+      })
+    })
 
     //when someone else connects
-    socket.on("user-connected", (userId) => {
-      console.log("user connected");
+    socket.on("user-connected", userId => {
+      console.log("user connected")
       setTimeout(function () {
-        connectToNewUser(userId, stream);
-      }, 1000);
-    });
+        connectToNewUser(userId, stream)
+      }, 1000)
+    })
 
-    socket.on("createMessage", (message) => {
-      console.log("message from our server", message);
-      const newMessage = document.createElement("li");
-      $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
-      console.log("updated message", newMessage);
-      scrollToBottom();
-    });
-  });
+    socket.on("createMessage", message => {
+      console.log("message from our server", message)
+      const newMessage = document.createElement("li")
+      $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`)
+      console.log("updated message", newMessage)
+      scrollToBottom()
+    })
+  })
 //connect to server and get user ID
-myPeer.on("open", (id) => {
-  socket.emit("join-room", ROOM_ID, id);
-});
+myPeer.on("open", id => {
+  socket.emit("join-room", ROOM_ID, id)
+})
 
 const connectToNewUser = (userId, stream) => {
-  console.log("The stream: ", stream);
-  console.log("User ID: ", userId);
-  const call = myPeer.call(userId, stream);
-  const video = document.createElement("video");
-  call.on("stream", (userVideoStream) => {
-    console.log("received their stream");
-    addVideoStream(video, userVideoStream);
-  });
+  console.log("The stream: ", stream)
+  console.log("User ID: ", userId)
+  const call = myPeer.call(userId, stream)
+  const video = document.createElement("video")
+  call.on("stream", userVideoStream => {
+    console.log("received their stream")
+    addVideoStream(video, userVideoStream)
+  })
   call.on("close", () => {
-    video.remove();
-  });
+    video.remove()
+  })
 
-  peers[userId] = call;
-};
+  peers[userId] = call
+}
 
 const addVideoStream = (video, stream) => {
-  video.srcObject = stream;
+  video.srcObject = stream
   video.addEventListener("loadedmetadata", () => {
-    video.play();
-  });
-  videoGrid.append(video);
-};
-let text = $("input");
-$("html").keydown((e) => {
+    video.play()
+  })
+  videoGrid.append(video)
+}
+let text = $("input")
+$("html").keydown(e => {
   if (e.which == 13 && text.val().length !== 0) {
-    socket.emit("message", text.val());
-    text.val("");
+    socket.emit("message", text.val())
+    text.val("")
   }
-});
+})
 const scrollToBottom = () => {
-  let d = $(".main__chat_window");
-  d.scrollTop(d.prop("scrollHeight"));
-};
+  let d = $(".main__chat_window")
+  d.scrollTop(d.prop("scrollHeight"))
+}
 const muteUnmute = () => {
-  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  const enabled = myVideoStream.getAudioTracks()[0].enabled
   if (enabled) {
-    myVideoStream.getAudioTracks()[0].enabled = false;
-    setUnmuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = false
+    setUnmuteButton()
   } else {
-    setMuteButton();
-    myVideoStream.getAudioTracks()[0].enabled = true;
+    setMuteButton()
+    myVideoStream.getAudioTracks()[0].enabled = true
   }
-};
+}
 
 const playStop = () => {
-  console.log("object");
-  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  console.log("object")
+  let enabled = myVideoStream.getVideoTracks()[0].enabled
   if (enabled) {
-    myVideoStream.getVideoTracks()[0].enabled = false;
-    setPlayVideo();
+    myVideoStream.getVideoTracks()[0].enabled = false
+    setPlayVideo()
   } else {
-    setStopVideo();
-    myVideoStream.getVideoTracks()[0].enabled = true;
+    setStopVideo()
+    myVideoStream.getVideoTracks()[0].enabled = true
   }
-};
+}
 const setMuteButton = () => {
   const html = `
     <i class="fas fa-microphone"></i>
     <span>Mute</span>
-  `;
-  document.querySelector(".main__mute_button").innerHTML = html;
-};
+  `
+  document.querySelector(".main__mute_button").innerHTML = html
+}
 
 const setUnmuteButton = () => {
   const html = `
     <i class="unmute fas fa-microphone-slash"></i>
     <span>Unmute</span>
-  `;
-  document.querySelector(".main__mute_button").innerHTML = html;
-};
+  `
+  document.querySelector(".main__mute_button").innerHTML = html
+}
 
 const setStopVideo = () => {
   const html = `
     <i class="fas fa-video"></i>
     <span>Stop Video</span>
-  `;
-  document.querySelector(".main__video_button").innerHTML = html;
-};
+  `
+  document.querySelector(".main__video_button").innerHTML = html
+}
 
 const setPlayVideo = () => {
   const html = `
   <i class="stop fas fa-video-slash"></i>
     <span>Play Video</span>
-  `;
-  document.querySelector(".main__video_button").innerHTML = html;
-};
+  `
+  document.querySelector(".main__video_button").innerHTML = html
+}
 // URL Copy To Clipboard
-document.getElementById("invite-button").addEventListener("click", getURL);
+document.getElementById("invite-button").addEventListener("click", getURL)
 
 function getURL() {
-  console.log("test1");
-  const c_url = window.location.href;
-  copyToClipboard(c_url);
-  alert("Url Copied to Clipboard,\nShare it with your Friends!\nUrl: " + c_url);
+  console.log("test1")
+  const c_url = window.location.href
+  copyToClipboard(c_url)
+  alert("Url Copied to Clipboard,\nShare it with your Friends!\nUrl: " + c_url)
 }
 
 function copyToClipboard(text) {
-  console.log("test2");
-  var dummy = document.createElement("textarea");
-  document.body.appendChild(dummy);
-  dummy.value = text;
-  dummy.select();
-  document.execCommand("copy");
-  document.body.removeChild(dummy);
+  console.log("test2")
+  var dummy = document.createElement("textarea")
+  document.body.appendChild(dummy)
+  dummy.value = text
+  dummy.select()
+  document.execCommand("copy")
+  document.body.removeChild(dummy)
 }
 // End Call
 // document
@@ -177,8 +177,29 @@ function copyToClipboard(text) {
 //   // window.location.href = "/";
 
 // }
-document.getElementById("end-button").addEventListener("click", endCall);
+document.getElementById("end-button").addEventListener("click", endCall)
 
 function endCall() {
-  window.location.href = "/";
+  window.location.href = "/"
 }
+
+$(document).ready(function () {
+  $("#chat-toggle").click(function () {
+    if (!$(".main__right").hasClass("main__right-toggle")) {
+      $(".main__right").addClass("main__right-toggle")
+    } else {
+      $(".main__right").removeClass("main__right-toggle")
+    }
+  })
+  window.addEventListener("click", function (e) {
+    if ($(".main__right").hasClass("main__right main__right-toggle")) {
+      if (document.getElementById("clickbox").contains(e.target)) {
+        // Clicked in box
+      } else {
+        if (!document.getElementById("chat-toggle").contains(e.target)) {
+          $(".main__right").removeClass("main__right-toggle")
+        }
+      }
+    }
+  })
+})
---
+++ readme.md
@@ -0,0 +1,44 @@
+# Speak Video Chat
+
+Video Webchat, a free video chatting website.
+
+## Demo
+
+https://video-webchat-backend.herokuapp.com/
+
+## Developed using
+
+- Node.js
+- Express.js
+- Socket.IO
+- PeerJs
+
+## How to install
+
+Clone this repo
+
+    git clone https://github.com/Kiruxg/Video-Chat-App-with-NodeJS.git
+
+    npm i to install all dependencies
+
+## How to use
+
+Run Server
+
+    node server
+
+Server Started on Port 3001.
+
+Run PeerJS Server in separate terminal.
+
+    peerjs --port 3001
+
+PeerJS Server Started on Port 3001.
+
+## Let's Start
+
+Open browser and go to http://localhost:3001/
+
+**Important :** Allow the camera and audio permissions asked by the browser.
+
+Enjoy the video chat!
